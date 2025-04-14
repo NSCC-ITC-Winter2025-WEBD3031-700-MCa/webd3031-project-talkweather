e8ddc1a0707dfc71f4f8323329e26c3aba3cbb84
Breanna Young <w0459556@campus.nscc.ca>
2025-04-14 11:36:00 -0300

    fix: alter upgrade button to work with db schema

--- src/app/(main)/_components/menu-bar.tsx
+++ src/app/(main)/_components/menu-bar.tsx
@@ -65,7 +65,7 @@ const MenuBar = async ({ className }: Props) => {
               </Link>
             </Button>
           );
-      } else if (user?.role !== "ADMIN" && user?.role !== "PREMIUM") {
+      } else if (user?.role !== "ADMIN" && !(user?.isVerified)) {
           return (
             <Button
             variant={"ghost"}
